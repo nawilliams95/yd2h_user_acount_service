bfa80b11038c443c0b9ed2197ae2f92ad3b9e450
Nykarri <karri.bonez@gmail.com>
2020-09-29 06:09:47 -0400

    heroku
    added corse and data to seed file for heroku deployment

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create([ 
+    {username: 'cutiewho', password: 'Christmas', first_name: 'CindyLu', last_name: 'Who', email: 'grinchlover@fake.com', avatar_img: 'https://i.ibb.co/VJ8wxMW/avatar-1577909-1280.webp'},
+    {username: 'peepingTom', password: 'Thomas', first_name: 'Thomas', last_name: 'Peeper', email: 'tommyboy@fake.com', avatar_img: 'https://i.ibb.co/VJ8wxMW/avatar-1577909-1280.webp'},
+    {username: 'greenmachine', password: 'IhateChristmas', first_name: 'The Grinch', last_name: 'Who stole Christmas', email: 'themeanone@fake.com', avatar_img: 'https://i.ibb.co/VJ8wxMW/avatar-1577909-1280.webp'}
+])
+
+puts "seeded database!!"
